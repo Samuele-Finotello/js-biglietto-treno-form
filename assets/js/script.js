480ec1dbd451673c78e3a8b3c07348b85e8970e3
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-10 23:58:11 +0200

    [add] feat definition variables

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const name = document.getElementById('name');
+const km = document.getElementById('km');
+const eta = document.getElementById('eta');
+const dateIn = document.getElementById('dateIn');
+const timeIn = document.getElementById('timeIn');
+const dateOut = document.getElementById('dateOut');
+const timeOut = document.getElementById('timeOut');
+const create = document.getElementById('create');
+const restart = document.getElementById('restart');
+const resultForm = document.getElementById('resultForm');
+
+const calcolaBiglietto = () => {
+  if ()
+}
