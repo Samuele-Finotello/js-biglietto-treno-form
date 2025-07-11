a5392127c3d44c4c166251c6bc7728d8c486b963
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-07-11 09:52:15 +0200

    [add] feat console.log info

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,36 @@
 const calcolaBiglietto = (anni, chilometri) => {
-  prezzo = chilometri * 0.21;
+  prezzoTotale = chilometri * 0.21;
   if (anni === 'Ho meno di 18 anni') {
-    sconto = prezzo * 20 / 100;
-    prezzoTotale = prezzo - sconto;
+    sconto = prezzoTotale * 20 / 100;
+    prezzoTotale = prezzoTotale - sconto;
   }
   else if (anni === 'Ho 65 o più anni') {
-    sconto = prezzo * 40 / 100;
-    prezzoTotale = prezzo - sconto;
+    sconto = prezzoTotale * 40 / 100;
+    prezzoTotale = prezzoTotale - sconto;
   }
+  return prezzoTotale;
 }
 
-const name = document.getElementById('name').value;
-const km = document.getElementById('km').value;
-const eta = document.getElementById('eta').value;
-const dateIn = document.getElementById('dateIn').value;
-const timeIn = document.getElementById('timeIn').value;
-const dateOut = document.getElementById('dateOut').value;
-const timeOut = document.getElementById('timeOut').value;
-const create = document.getElementById('create').value;
-const restart = document.getElementById('restart').value;
+const create = document.getElementById('create');
+
+create.addEventListener("click", function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const km = document.getElementById('km').value;
+  const eta = document.getElementById('eta').value;
+  const dateIn = document.getElementById('dateIn').value;
+  const timeIn = document.getElementById('timeIn').value;
+  const dateOut = document.getElementById('dateOut').value;
+  const timeOut = document.getElementById('timeOut').value;
+  console.log(name);
+  console.log(km);
+  console.log(eta);
+  console.log(dateIn);
+  console.log(timeIn);
+  console.log(dateOut);
+  console.log(timeOut);
+})
+
 const resultForm = document.getElementById('resultForm').value;
 
